91154c66efe350158c3101b5f88296b5f92427cd
Anderson Chen <chenzhengyang070@gmail.com>
2025-02-28 07:44:22 +0000

    new version

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,30 @@
-import Header from './components/header'
-import './globals.css'
-import { Inter, Press_Start_2P } from 'next/font/google'
+import Header from "./components/header";
+import "./globals.css";
+import { Inter, Press_Start_2P } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
-const pixelFont = Press_Start_2P({ 
-  weight: '400',
-  subsets: ['latin'],
-  variable: '--font-pixel'
-})
+const pixelFont = Press_Start_2P({
+  weight: "400",
+  subsets: ["latin"],
+  variable: "--font-pixel",
+});
 
 export const metadata = {
-  title: 'Anderson C.',
-  description: 'Portfolio website',
-}
+  title: "Anderson C.",
+  description: "Portfolio website",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} ${pixelFont.variable}`}> 
-        <Header />
-        <main className="pt-16">{children}</main>
+      <body className={`${inter.className} ${pixelFont.variable}`}>
+        <main className="">{children}</main>
       </body>
     </html>
-  )
+  );
 }
--- app/pages/projects/page.tsx
+++ app/pages/projects/page.tsx
@@ -1,6 +1,8 @@
-'use client'
-import { useState } from 'react';
-import Currentlyat from '@/app/little-stuff/currentlyat';
+"use client";
+import { useState } from "react";
+import Currentlyat from "@/app/little-stuff/currentlyat";
+import DynamicFrameLayout from "@/designcomposnent/DynamicFrameLayout";
+import HoverEffect from "@/app/designcomponents/folder";
 
 export default function Projects() {
   const [headerSize, setHeaderSize] = useState(1);
@@ -10,43 +12,59 @@ export default function Projects() {
         {/*Left Section*/}
         <div className="w-full md:w-[300px] flex-shrink-0 flex flex-col justify-between h-full">
           <div className="flex flex-col gap-5">
-            <h1 className="text-4xl md:text-6xl font-light 
+            <h1
+              className="text-4xl md:text-6xl font-light 
             space-y-4 italic text-white/80 tracking-tighter leading-[130%]"
-              style={{fontSize: `${4*headerSize}rem`}}>
-                Anderson Chen
+              style={{ fontSize: `${4 * headerSize}rem` }}
+            >
+              Anderson Chen
             </h1>
             <h2 className="text-sm font-light text-white/80 text-center pt-6">
-            <b>Chemical Engineering & Business</b> at <p className="hover:text-red-400">Nanyang Technological University & EPFL</p>
+              <b>Chemical Engineering & Business</b> at{" "}
+              <p className="hover:text-red-400">
+                Nanyang Technological University & EPFL
+              </p>
             </h2>
             <div className="space-y-4 pt-6">
-              <div className="h-px bg-white/10 w-full"/>
+              <div className="h-px bg-white/10 w-full" />
               <Currentlyat />
             </div>
             <div className="h-pix font-light text-xs text-white/60">
               <div className="md:w-[310px] pl-4">
                 <p className="leading-[200%]">
-                  Hi! I'm Anderson, 
-                  I was an aspiring artist who decided to pursue a career in 
-                  chemical engineering 5 months before high school graduation because 
-                  I fell in love with differential equation and biology. 
-                  In college, I had to study classes like organic chemistry, physics with 
-                  no prior knowledge from high school, but these experiences helped me to 
-                  build a system for learning and adapting to new knowledge fast.
-                  I'm currently teaching myself how to code and design things people want. 
-                  Taking a gap year from 2025 July to 2026 July to fully invest myself in the world of tech! 
-                  <br/>
-                  <br/>
-
-                  In my spare time, I like to {""} <a href="https://youtu.be/G4VDvKo2B8U" className="text-red-400 hover:text-red-200">  
-                  <b>listen & make podcasts</b></a>, read history books, play basketball. 
-                  and talk to different people. Sometimes I yap on my blog. Feel free to reach out to me :D
+                  Hi! I'm Anderson, I was an aspiring artist who decided to
+                  pursue a career in chemical engineering 5 months before high
+                  school graduation because I fell in love with differential
+                  equation and biology. In college, I had to study classes like
+                  organic chemistry, physics with no prior knowledge from high
+                  school, but these experiences helped me to build a system for
+                  learning and adapting to new knowledge fast. I'm currently
+                  teaching myself how to code and design things people want.
+                  Taking a gap year from 2025 July to 2026 July to fully invest
+                  myself in the world of tech!
+                  <br />
+                  <br />
+                  In my spare time, I like to {""}{" "}
+                  <a
+                    href="https://youtu.be/G4VDvKo2B8U"
+                    className="text-red-400 hover:text-red-200"
+                  >
+                    <b>listen & make podcasts</b>
+                  </a>
+                  , read history books, play basketball. and talk to different
+                  people. Sometimes I yap on my blog. Feel free to reach out to
+                  me :D
                 </p>
               </div>
             </div>
           </div>
         </div>
+        {/*Right Section*/}
+        <div className="w-full md:w-[700px] justify-between bg-sky-200 flex h-full"></div>
+        <div className="px-5">
+          <HoverEffect />
+        </div>
       </div>
-      {/*Right Section*/}
-    </div>   
-  )
-}   
+    </div>
+  );
+}
