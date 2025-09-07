6c50c55b1609aa651df7d3d76f665323c25ab384
Anderson Chen <chenzhengyang070@gmail.com>
2025-09-07 19:25:32 +0800

    vercel speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Inter, Press_Start_2P } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 const pixelFont = Press_Start_2P({
@@ -22,6 +23,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} ${pixelFont.variable}`}>
         <main>{children}</main>
+        <SpeedInsights />
       </body>
     </html>
   );
